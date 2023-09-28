42246c8c10c89cd2724b7ff9d59001b127dbfcf9
mcthouacbb <Mcthouacbb@gmail.com>
2023-09-28 14:03:14 +0000

    Added basic headers

---
+++ src/bitboard.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+#include <iostream>
+#include <bitset>
+#include <bit>
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+#endif
+
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+using Bitboard = uint64_t;
+
+inline uint32_t getLSB(Bitboard bb)
+{
+#if defined(__GNUC__) || __has_builtin(__builtin_ctzll)
+	return __builtin_ctzll(bb);
+#elif defined(_MSC_VER)
+	unsigned long idx;
+	_BitScanForward64(&idx, bb);
+	return idx;
+#else
+    return std::countr_zero(bb);
+#endif
+}
+
+inline uint32_t getMSB(Bitboard bb)
+{
+#if defined(__GNUC__) || __has_builtin(__builtin_clzll)
+	return 63 - __builtin_clzll(bb);
+#elif defined(_MSC_VER)
+	unsigned long idx;
+	_BitScanReverse64(&idx, bb);
+	return idx;
+#else
+    return 63 - std::countl_zero(bb);
+#endif
+}
+
+inline uint32_t popLSB(Bitboard& bb)
+{
+    uint32_t lsb = getLSB(bb);
+    bb &= bb - 1;
+    return lsb;
+}
+
+inline uint32_t popcount(Bitboard bb)
+{
+    return std::popcount(bb);
+}
---
+++ src/defs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class Color
+{
+    RED,
+    YELLOW
+};
+
+constexpr Color flip(Color c)
+{
+    return static_cast<Color>(static_cast<int>(c) ^ 1);
+}
+
+struct Move
+{
+    uint16_t sqIdx;
+    uint16_t column;
+};
